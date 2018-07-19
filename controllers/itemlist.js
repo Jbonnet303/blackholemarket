bfd8f47d9485cc528c586c63b18dd66f7483cb3f
Brantley Burton <brantleyburton@Brantleys-MacBook-Air.local>
2018-07-19 14:43:55 -0400

    added errors to itemlist controller

--- controllers/itemlist.js
+++ controllers/itemlist.js
@@ -11,19 +11,46 @@ router.get('/', (req, res)=>{
 //Deletes the item
 router.delete('/:id', (req, res)=>{
     Itemlist.findByIdAndRemove(req.params.id, (err, deletedItem)=>{
-        res.json(deletedItem);
+      if(err){
+        //Failure to delete item
+        console.log('Failure to delete item:', err)
+        res.status(500).json({
+          status: 500,
+          message: 'Failed to delete item with id:' + request.params.id
+        })
+      } else {
+          res.json(deletedItem);
+      }
     });
 });
 //Creates new item
 router.post('/', (req, res)=>{
     Itemlist.create(req.body, (err, createdItem)=>{
-        res.json(createdItem);
+      if(err){
+        //Failure to create item
+        console.log('Failure to create item:', err)
+        res.status(500).json({
+          status: 500,
+          message: 'Failed to create item with id:' + request.params.id
+        })
+      } else {
+          res.json(createdItem);
+      }
     });
 });
-//Adds item to list of items
+//Updates item
 router.put('/:id', (req, res)=>{
     Itemlist.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedItem)=>{
-        res.json(updatedItem);
+      if(err){
+        //Failure to post item
+        console.log('Failure to update item:', err)
+        res.status(500).json({
+          status: 500,
+          message: 'Failed to update item with id:' + request.params.id
+        })
+      } else {
+          res.json(updatedItem);
+      }
     });
 });
 
