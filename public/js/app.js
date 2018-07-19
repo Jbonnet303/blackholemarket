5c86a679f1022cf776a3e89b83173359d5325498
Keith Swanson <3818627+Psycoder42@users.noreply.github.com>
2018-07-19 13:14:49 -0500

    created bare bones angular app

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+// Create the angular app
+const app = angular.module("BHMApp", []);
+
+// The main controller
+app.controller("MainController", function() {
+  // A trick to make referencing controller variables the same
+  // from the index.html and inside the folder
+  const ctrl = this;
+  // All references from now on will user ctrl.<ref> instead of
+  // this.<ref>
+  ctrl.title = "Welcome to the Blackhole Market!";
+})
