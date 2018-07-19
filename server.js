e3b10666dfde7d41cf5b8f2145d640ec81dbd51a
Brantley Burton <brantleyburton@Brantleys-MacBook-Air.local>
2018-07-19 15:10:48 -0400

    added items controller to server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(express.static('public'));
 
+//Connects itemlist controller
+const itemlistController = require('./controllers/itemlist.js');
+app.use('/items', itemlistController);
+
 // Start the server
 app.listen(port, ()=>{
   console.log("Server listening on port", port);
